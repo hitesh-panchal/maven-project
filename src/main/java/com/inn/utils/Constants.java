98a6087e4b69923ef8fd4ce521367d4b87385c38
hitesh-panchal <56149142+hitesh-panchal@users.noreply.github.com>
2019-10-04 12:30:42 +0530

    Add files via upload

---
+++ src/main/java/com/inn/utils/Constants.java
@@ -0,0 +1,35 @@
+package com.inn.utils;
+/*-
+ * #%L
+ * thinkbig-commons-util
+ * %%
+ * Copyright (C) 2017 ThinkBig Analytics
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+public class Constants {
+	 public static final String NotProcessNDashboard = "NotProcessNDashboard";
+	 public static final String ProcessNDashboard = "ProcessNDashboard";
+	 public static final String nonKafkaModule = "nonKafkaModule";
+	 public static final String kafkaModule = "kafkaModule";
+	 public static final String jobName = "jobName";
+	 public static final String groupName = "groupName";
+	 public static final String topicName = "topicName";
+	 public static final String zookeeperIp = "zookeeperIp";
+	 public static final String zookeeperPort = "zookeeperPort";
+	 public static final String kafkaTopicCommand = "kafkaTopicCommand";
+	 public static final String kafkaGroupCommand = "kafkaGroupCommand";
+	public static final String STAGING = "staging";
+	public static final String LOCAL = "local";
+}
